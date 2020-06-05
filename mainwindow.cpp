c013d12b4754e89303ee37d6f43b56c993221a8d
Chrizanne <61121324+Chrizanne@users.noreply.github.com>
2020-06-05 20:32:42 +0200

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,56 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
+// EXPLANATION , JUST COMMENT OUT TO SEE PROGRAM EXAMPLE.
 
-MainWindow::~MainWindow()
+// QLinkedList<T> is one of Qt's generic container classes.
+//It stores a list of values and provides iterator-based access as well as constant time insertions and removals.
+//QList<T>, QLinkedList<T>, and QVector<T> provide similar functionality.
+//Here's an overview:
+//For most purposes, QList is the right class to use. Its index-based API is more convenient than QLinkedList's iterator-based API, and it is usually faster than QVector because of the way it stores its items in memory (see Algorithmic Complexity for details). It also expands to less code in your executable.
+//If you need a real linked list, with guarantees of constant time insertions in the middle of the list and iterators to items rather than indexes, use QLinkedList.
+//If you want the items to occupy adjacent memory positions, use QVector.
+
+ //Here's an example of a QLinkedList that stores integers and a QLinkedList that stores QTime values:
+#include <QLinkedList>
+QLinkedList<int> integerList;
+QLinkedList<QTime> timeList
+
+//QLinkedList stores a list of items. The default constructor creates an empty list. To insert items into the list, you can use operator
+
+QLinkedList<QString> list;
+        list << "one" << "two" << "three";
+        // list: ["one", "two", "three"]
+
+  // If you want to get the first or last item in a linked list, use first() or last(). If you want to remove an item from either end of the list, use removeFirst() or removeLast(). If you want to remove all occurrences of a given value in the list, use removeAll().
+ // A common requirement is to remove the first or last item in the list and do something with it. For this, QLinkedList provides takeFirst() and takeLast(). Here's a loop that removes the items from a list one at a time and calls delete on them:
+        QLinkedList<QWidget *> list;
+        ...
+        while (!list.isEmpty())
+            delete list.takeFirst();
+
+ //QLinkedList's value type must be an assignable data type. This covers most data types that are commonly used, but the compiler won't let you, for example, store a QWidget as a value; instead, store a QWidget *. A few functions have additional requirements; for example, contains() and removeAll() expect the value type to support operator==(). These requirements are documented on a per-function basis
+
+
+        //PROGRAM 1 EXAMPLE
+        //QLinkedList stores a list of items. The default constructor creates an empty list. To insert items into the list, we can use operator<<().
+
+#include <QCoreApplication>
+#include <QLinkedList>
+#include <QDebug>
+#include <QString>
+
+int main(int argc, char *argv[])
 {
-    delete ui;
+    QCoreApplication a(argc, argv);
+
+    QLinkedList<QString> List;
+
+    List << "A" << "B" << "C";
+    List.append("D");
+    List.append("E");
+    List.append("F");
+
+    foreach(QString s, List) qDebug() << s;
+
+    return a.exec();
 }
 
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.8.0
+** Created by: Qt User Interface Compiler version 5.12.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -10,15 +10,9 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
-#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QFrame>
-#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -28,98 +22,28 @@ QT_BEGIN_NAMESPACE
 class Ui_MainWindow
 {
 public:
-    QAction *actionNew;
-    QAction *actionWire;
-    QAction *actionComponents;
-    QAction *actionSimulate;
-    QAction *actionExit;
-    QAction *actionOpen;
-    QWidget *centralWidget;
-    QFrame *frame;
-    QPushButton *Components;
-    QPushButton *Wire;
-    QPushButton *Simulate;
     QMenuBar *menuBar;
-    QMenu *menuFile;
     QToolBar *mainToolBar;
+    QWidget *centralWidget;
     QStatusBar *statusBar;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
-            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(400, 300);
-        actionNew = new QAction(MainWindow);
-        actionNew->setObjectName(QStringLiteral("actionNew"));
-        QIcon icon;
-        icon.addFile(QStringLiteral(":/images/icons8-add-50.png"), QSize(), QIcon::Normal, QIcon::Off);
-        actionNew->setIcon(icon);
-        actionWire = new QAction(MainWindow);
-        actionWire->setObjectName(QStringLiteral("actionWire"));
-        QIcon icon1;
-        icon1.addFile(QStringLiteral(":/images/icons8-wire-50.png"), QSize(), QIcon::Normal, QIcon::Off);
-        actionWire->setIcon(icon1);
-        actionComponents = new QAction(MainWindow);
-        actionComponents->setObjectName(QStringLiteral("actionComponents"));
-        QIcon icon2;
-        icon2.addFile(QStringLiteral(":/images/icons8-heater-resistor-50.png"), QSize(), QIcon::Normal, QIcon::Off);
-        actionComponents->setIcon(icon2);
-        actionSimulate = new QAction(MainWindow);
-        actionSimulate->setObjectName(QStringLiteral("actionSimulate"));
-        QIcon icon3;
-        icon3.addFile(QStringLiteral(":/images/icons8-electronics-50.png"), QSize(), QIcon::Normal, QIcon::Off);
-        actionSimulate->setIcon(icon3);
-        actionExit = new QAction(MainWindow);
-        actionExit->setObjectName(QStringLiteral("actionExit"));
-        actionOpen = new QAction(MainWindow);
-        actionOpen->setObjectName(QStringLiteral("actionOpen"));
-        QIcon icon4;
-        icon4.addFile(QStringLiteral(":/images/icons8-folder-50.png"), QSize(), QIcon::Normal, QIcon::Off);
-        actionOpen->setIcon(icon4);
-        centralWidget = new QWidget(MainWindow);
-        centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        frame = new QFrame(centralWidget);
-        frame->setObjectName(QStringLiteral("frame"));
-        frame->setGeometry(QRect(10, 10, 311, 81));
-        frame->setFrameShape(QFrame::StyledPanel);
-        frame->setFrameShadow(QFrame::Raised);
-        Components = new QPushButton(frame);
-        Components->setObjectName(QStringLiteral("Components"));
-        Components->setGeometry(QRect(162, 10, 61, 61));
-        Wire = new QPushButton(frame);
-        Wire->setObjectName(QStringLiteral("Wire"));
-        Wire->setGeometry(QRect(86, 10, 61, 61));
-        Simulate = new QPushButton(frame);
-        Simulate->setObjectName(QStringLiteral("Simulate"));
-        Simulate->setGeometry(QRect(10, 10, 61, 61));
-        Components->raise();
-        Wire->raise();
-        Simulate->raise();
-        Components->raise();
-        Wire->raise();
-        Simulate->raise();
-        MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
-        menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 400, 26));
-        menuFile = new QMenu(menuBar);
-        menuFile->setObjectName(QStringLiteral("menuFile"));
+        menuBar->setObjectName(QString::fromUtf8("menuBar"));
         MainWindow->setMenuBar(menuBar);
         mainToolBar = new QToolBar(MainWindow);
-        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
-        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        mainToolBar->setObjectName(QString::fromUtf8("mainToolBar"));
+        MainWindow->addToolBar(mainToolBar);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
+        MainWindow->setCentralWidget(centralWidget);
         statusBar = new QStatusBar(MainWindow);
-        statusBar->setObjectName(QStringLiteral("statusBar"));
+        statusBar->setObjectName(QString::fromUtf8("statusBar"));
         MainWindow->setStatusBar(statusBar);
-
-        menuBar->addAction(menuFile->menuAction());
-        menuFile->addAction(actionNew);
-        menuFile->addAction(actionOpen);
-        menuFile->addAction(actionWire);
-        menuFile->addAction(actionComponents);
-        menuFile->addAction(actionSimulate);
-        menuFile->addSeparator();
-        menuFile->addAction(actionExit);
 
         retranslateUi(MainWindow);
 
@@ -128,23 +52,7 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
-        actionNew->setText(QApplication::translate("MainWindow", "New", Q_NULLPTR));
-        actionNew->setShortcut(QApplication::translate("MainWindow", "Ctrl+N", Q_NULLPTR));
-        actionWire->setText(QApplication::translate("MainWindow", "Wire", Q_NULLPTR));
-        actionWire->setShortcut(QApplication::translate("MainWindow", "W", Q_NULLPTR));
-        actionComponents->setText(QApplication::translate("MainWindow", "Components", Q_NULLPTR));
-        actionComponents->setShortcut(QApplication::translate("MainWindow", "C", Q_NULLPTR));
-        actionSimulate->setText(QApplication::translate("MainWindow", "Simulate", Q_NULLPTR));
-        actionSimulate->setShortcut(QApplication::translate("MainWindow", "Ctrl+S", Q_NULLPTR));
-        actionExit->setText(QApplication::translate("MainWindow", "Exit", Q_NULLPTR));
-        actionExit->setShortcut(QApplication::translate("MainWindow", "Esc", Q_NULLPTR));
-        actionOpen->setText(QApplication::translate("MainWindow", "Open", Q_NULLPTR));
-        actionOpen->setShortcut(QApplication::translate("MainWindow", "Ctrl+O, Return", Q_NULLPTR));
-        Components->setText(QString());
-        Wire->setText(QString());
-        Simulate->setText(QString());
-        menuFile->setTitle(QApplication::translate("MainWindow", "File", Q_NULLPTR));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
     } // retranslateUi
 
 };
